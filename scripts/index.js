53f5bf712c287bade7e540358ad4c695c0879384
Michael "Cal" Krug <wolfdaemon@murena.io>
2024-12-24 04:24:42 -0500

    feat: func for mobile menu, site styling
    - Add a button for a mobile menu in order to toggle said menu on & off
      dynamically
    - Add styling for the whole site

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@
 const qualities = ["Wise", "Courageous", "Precise", "Authoritative", "Creative", "Premium", "Unprecedented", "Pivotal", "Hot", "Elite", "Essential", "Today", "Contemporary", "Genuine", "Significant", "Affirmative", "Compelling", "Passionate"];
 const captionIAm = document.querySelector("#caption-i-am");
 
+// functions
+
 function chooseQualities() {
 	// choose a random array word
 	randInt = Math.floor(Math.random() * (qualities.length)); // return random number from 0 to provided array length
@@ -10,5 +12,18 @@ function chooseQualities() {
 	//append quality to paragraph
 	captionIAm.textContent = qualities[randInt];
 }
+
+function toggleDisplayMenu() {
+	const mobileMenu = document.querySelector(".mobile-menu");
+	const mobileMenuDisplay = mobileMenu.style.display; 
+	if (mobileMenuDisplay == "block") {
+		mobileMenu.style.display = "none";
+	} else {
+		mobileMenu.style.display = "block";
+	}
+
+}
+
+// function calls
 
 setInterval(chooseQualities, 200); // run function every 0.2 seconds
